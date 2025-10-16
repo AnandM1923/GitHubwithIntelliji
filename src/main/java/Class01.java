e34da743604f9b48ba858ba93c32643c047b15dc
margolanand@gmail.com <Shlok@1923>
2025-10-16 11:03:16 +0530

    "Created Class01 and added Class01 in the print statement "

--- src/main/java/Class01.java
+++ src/main/java/Class01.java
@@ -1,6 +1,6 @@
 public class Class01 {
     public static void main(String[] args) {
-        System.out.println("Hello World");
-        System.out.println("Welcome to Java Programming");
+        System.out.println("Hello World Class01");
+        System.out.println("Welcome to Java Programming Class01");
     }
 }
---
+++ src/main/java/Class02.java
@@ -0,0 +1,6 @@
+public class Class02 {
+    public static void main(String[] args) {
+        System.out.println("Hello World Class02");
+        System.out.println("Welcome to Java Programming Class02");
+    }
+}
